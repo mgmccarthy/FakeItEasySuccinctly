18bb7c8b88f47dda4d3b3a2f1c5e6cd32c38c5b6
indiecodemonkey <mike_mccarthy3@yahoo.com>
2015-09-07 20:56:57 -0400

    FakeItEasy Succinctly

---
+++ FakeItEasySuccinctly/Chapter4CreatingAFake/FakingAClass.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using FakeItEasy;
+using NUnit.Framework;
+
+namespace FakeItEasySuccinctly.Chapter4CreatingAFake
+{
+    public interface IAmAnInterface
+    {
+        void DoSomething();
+        string DoSomethingAndReturnAString();
+        List<string> DoSomethingAndReturnAListOfString();
+    }
+
+    [TestFixture]
+    public class WhenFakingAnInterface
+    {
+        [SetUp]
+        public void Given()
+        {
+            var aFakeInterface = A.Fake<IAmAnInterface>();
+        }
+    }
+
+    public class AClass
+    {
+        public virtual void DoSomething()
+        {
+            //some implementation
+        }
+    }
+
+    [TestFixture]
+    public class WhenFakingAClass
+    {
+        [SetUp]
+        public void Given()
+        {
+            var aFakeClass = A.Fake<AClass>();
+        }
+    }
+}
